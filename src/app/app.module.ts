11f0d8ef5922e658f936b5cc0a6851793f1f3f14
Stephen Fluin <stephen.fluin@gmail.com>
2017-07-17 09:59:04 -0700

    Setup initial app with routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { StatsComponent } from './stats/stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule,
